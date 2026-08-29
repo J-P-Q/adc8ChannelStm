13f89633cb425b13ffe8d42f50199117c2dae67c
jp <que.johnn0@gmail.com>
2026-08-29 21:00:11 +0800

    test timer

--- src/main.c
+++ src/main.c
@@ -1,11 +1,24 @@
 #include <stm32f401xc.h>
 #include <stdint.h>
 
-void main(void){
+#include <tmr2.h>
+
+volatile uint8_t msCounter = 0;
+
+int main(void){
     RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOCEN;
+
+    GPIOC -> MODER |= (1 << 13*2);
+
+    tmr2_init();
     while(1){
+
 
     }
 
-    return;
+    return 0;
+}
+
+void TIM2_IQRHandler(void){
+    GPIOC -> ODR ^= (1 << 13);
 }
